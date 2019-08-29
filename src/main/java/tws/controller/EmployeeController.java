e172324121b610198b8c8bdd4c7b87a02c9e64cc
threey-y <threey.y@foxmail.com>
2019-08-29 23:01:11 +0800

    first commit

--- src/main/java/tws/controller/EmployeeController.java
+++ src/main/java/tws/controller/EmployeeController.java
@@ -1,11 +1,15 @@
 package tws.controller;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import tws.entity.Employee;
 import tws.repository.EmployeeMapper;
@@ -20,14 +24,37 @@ public class EmployeeController {
     @Autowired
     private EmployeeMapper employeeMapper;
 
-    @GetMapping("")
-    public ResponseEntity<List<Employee>> getAll() {
-        return ResponseEntity.ok(employeeMapper.selectAll());
-    }
-
-    @PostMapping("")
-    public ResponseEntity<Employee> insert(@RequestBody Employee employee) {
-        employeeMapper.insert(employee);
-        return ResponseEntity.created(URI.create("/employees/" + employee.getId())).body(employee);
-    }
+//    @GetMapping("")
+//    @ResponseStatus(HttpStatus.OK)
+//    public ResponseEntity<List<Employee>> getAll() {
+//        return ResponseEntity.ok(employeeMapper.selectAll());
+//    }
+//
+//    @PostMapping("")
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public ResponseEntity<Employee> insert(@RequestBody Employee employee) {
+//        employeeMapper.insert(employee);
+//        return ResponseEntity.created(URI.create("/employees/" + employee.getId())).body(employee);
+//    }
+    
+    
+    
+    
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void addEmployee(@RequestBody Employee employee) {
+		employeeMapper.insertEmployee(employee);
+	}
+	
+	@GetMapping 
+	@ResponseStatus(HttpStatus.OK)
+	public List<Employee> serchEmployees(){
+		return employeeMapper.searchEmployees();
+	}
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void updateEmployee(@Param("id") int id,@RequestBody Employee employee) {
+		employeeMapper.updateEmployee(id,employee);
+	}
 }
--- src/main/java/tws/repository/EmployeeMapper.java
+++ src/main/java/tws/repository/EmployeeMapper.java
@@ -1,13 +1,24 @@
 package tws.repository;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 import tws.entity.Employee;
 
 import java.util.List;
 
 @Mapper
 public interface EmployeeMapper {
-    List<Employee> selectAll();
-   void insert(@Param("employee") Employee employee);
+   
+   @Insert("insert into employee values(#{employee.id},#{employee.name},#{employee.age})")
+	void insertEmployee(@Param("employee") Employee employee);
+	
+	@Select("select * from employee")
+	List<Employee> searchEmployees();
+
+	@Update("update ")
+	void updateEmployee(Employee employee);
 }
